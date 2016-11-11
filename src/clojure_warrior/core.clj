67d3137b8d1155a9dcb9056927383be911a1dfdb
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-11-10 22:25:07 -0500

    Simplify generate-display to make use of units :display-char

--- src/clojure_warrior/core.clj
+++ src/clojure_warrior/core.clj
@@ -1,8 +1,7 @@
 (ns clojure-warrior.core
   (:gen-class)
   (:require
-    [clojure.string :as string]
-    [clojure-warrior.units :as units]))
+    [clojure.string :as string]))
 
 (defn generate-display [state]
   (let [width (count (first (state :board)))
@@ -14,6 +13,6 @@
                    (map (fn [row]
                           (->> row
                                (map (fn [space]
-                                      (:display-char (units/reference (space :type)))))
+                                      (:display-char space)))
                                (string/join "")))))
               [line]))))
--- test/clojure_warrior/test/core.clj
+++ test/clojure_warrior/test/core.clj
@@ -6,30 +6,15 @@
 (deftest generate-display
   (testing "generate-display"
     (let [in {:board
-              [[{:type :wall}
-                {:type :stairs}
+              [[{:type :wall
+                 :display-char \|}
                 {:type :captive
-                 :direction :east
-                 :health 1.0}
+                 :display-char \C}
                 {:type :archer
-                 :direction :east
-                 :health 7.0}
-                {:type :floor}
-                {:type :floor}
-                {:type :warrior
-                 :health 20.0
-                 :direction :east}
-                {:type :floor}
-                {:type :thick-sludge
-                 :direction :west
-                 :health 24.0}
-                {:type :floor}
-                {:type :wizard
-                 :direction :west
-                 :health 3.0}
-                {:type :captive
-                 :direction :west
-                 :health 1.0}
-                {:type :wall}]]}
-          out "-------------\n|>Ca  @ S wC|\n-------------"]
+                 :display-char \a}
+                {:type :floor
+                 :display-char " "}
+                {:type :wall
+                 :display-char \|}]]}
+          out "-----\n|Ca |\n-----"]
       (is (= out (core/generate-display in))))))
